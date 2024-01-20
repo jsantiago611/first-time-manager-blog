3ff31c52c282094edab3c6bbb2bbd612e1a5533b
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2024-01-19 21:02:01 -0500

    Update site-config.ts

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -38,7 +38,7 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    title: 'First-time Managers Only',
+    title: 'The First-time Manager Journal',
     subtitle: 'Business and Management Advice for First-time Managers and Supervisors',
     description: 'Business and Management Advice for First-time Managers and Supervisors',
     image: {
@@ -92,7 +92,7 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Serious Managers Only!',
+        title: 'Skyrocket your Newfound Management Career',
         text: "Hey, I'm **Jonathan Santiago**, a Production Support Supervisor at one of the fastest-growing insurance companies in the US. This is the first time in my career that I've held a management position, and I'm recording everything I learn as I rub shoulders with the brightest people in Insurance. Learn with me as we grow on this journey together!",
         image: {
             src: '/hero.jpeg',
